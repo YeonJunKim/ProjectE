fe5cfcc5589e61b40500ff73da44937e377bf139
Jaderidge <heyoka1@naver.com>
2018-08-11 03:45:39 -0700

    sensor - app connect and real-time transfer complete. real-time air not complete

--- app/src/main/java/com/example/yeonjun/uidesign/RealTimeFragment.java
+++ app/src/main/java/com/example/yeonjun/uidesign/RealTimeFragment.java
@@ -59,7 +59,7 @@ public class RealTimeFragment extends Fragment {
             }
         }
     };
-    
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
